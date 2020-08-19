287bf4e3cd9467354be92c389f2a2bb551e702d1
0x024 <97073966@qq.com>
2020-08-19 23:01:54 +0800

    upload

---
+++ backup/etm.py
@@ -0,0 +1,315 @@
+import sqlite3
+import os,email,imaplib
+from email.parser import BytesParser
+from email.utils import parseaddr 
+from lxml import etree
+import time
+import xml.etree.ElementTree as ET
+
+aaaa=1
+
+
+host = 'imap.qq.com'
+user = '97073966@qq.com' 
+passwd = 'feaybkuahvcacajc'
+raw_conn = imaplib.IMAP4_SSL(host) 
+raw_conn.login(user,passwd) 
+raw_conn.select()
+type,data=raw_conn.search(None,'ALL')
+email_list=data[0].split()
+print (len(email_list))
+
+def decode_str(s):
+	try:
+		subject=email.header.decode_header(s)
+	except:
+		return None
+	sub_bytes=subject[0][0]
+	sub_charset=subject[0][1]
+	if None==sub_charset:
+		subject=sub_bytes
+	elif 'unknown-8bit'==sub_charset:
+		subject=str(sub_bytes,'utf-8')
+	else:
+		subject=str(sub_bytes,sub_charset)
+	return subject
+
+def get_transfer_v1(content):
+	a1=content.replace('&nbsp;','')
+	a2=a1.replace('<a <a ','<a ')
+	a3=a2.replace('(<a href="http://www.12306.cn">www.12306.cn</a>)','')
+	a4=a3.replace('（<a href="http://www.12306.cn">www.12306.cn</a>)','')
+	a5=a4.replace('（<a href="http://www.12306.cn">12306.cn</a>)','')
+	a6=a5.replace('(<a href="http://www.12306.cn">12306.cn</a>)','')
+	tree=etree.HTML(a6)
+	contents=tree.xpath('//text()')
+	''.join(contents)
+	line_chick=contents[2].replace("\n",'')
+	list_chick=line_chick.split('，')
+	print (line_chick)
+	order_type=list_chick[0]
+	if'购买' in order_type :
+		order_count=int(list_chick[0][-4])
+		if order_count==2:
+			print ('!!!!!!!!!!!!!!!!!!!!!!!!!')
+		count =1
+		while count < order_count+1:
+
+			line_detail=contents[count+2].replace("\n",'')
+			list_chick=line_chick.split('，')
+			print(list_chick)
+			if ',' in line_detail:
+				list_detail=line_detail.split(',')
+			elif '，' in line_detail:
+				list_detail=line_detail.split('，')
+			print (list_detail)
+			print ("购买")
+			purchaser='张文'
+			print(purchaser)
+			order_no=list_chick[2][4:].split('。')[0]
+			print(order_no)
+			order_date=list_chick[0][2:13]
+			print (order_date)
+			order_price=list_chick[1][4:]
+			print (order_price)
+			order_type="购买"
+			print (order_type)
+			passenger=list_detail[0].split('.')[1]
+			print (passenger)
+			train_date=list_detail[1][:-1]
+			print (train_date)
+			train_price=list_detail[6][2:]
+			print (train_price)
+			train_no=list_detail[3].replace("次列车",'')
+			print (train_no)
+			train_type=list_detail[3][0]
+			if train_type.isdigit():
+				print ("normla")
+			else:
+				print (train_type)
+			start_station=list_detail[2].split('—')[0]
+			print (start_station)
+			stop_station=list_detail[2].split('—')[1]
+			print (stop_station)
+			sit_row=list_detail[4].split("车")[0]
+			print (sit_row)
+			sit_no=list_detail[4].split("车")[1].split("号")[0]
+			print (sit_no)
+			sit_type=list_detail[5]
+			print (sit_type)
+			if sit_type=="硬卧":
+				sit_flow=list_detail[4][-2]
+				print (sit_flow)
+			count=count+1
+			
+
+
+	elif '退票' in order_type:
+		line_detail=contents[3].replace("\n",'')
+		list_chick=line_chick.split('，')
+		print(list_chick)
+		if ',' in line_detail:
+			list_detail=line_detail.split(',')
+		elif '，' in line_detail:
+			list_detail=line_detail.split('，')
+		print (list_detail)
+		print ("退票")
+		purchaser='张文'
+		print(purchaser)
+		order_no=list_chick[1].split('。')[0][5:]
+		print(order_no)
+		order_date=list_chick[0][2:13]
+		print (order_date)
+		#order_price=list_chick[1][4:]
+		#print (order_price)
+		order_type="退票"
+		print (order_type)
+		passenger=list_detail[0]
+		print (passenger)
+		train_date=order_date[0:5]+list_detail[1]
+		print (train_date)
+		train_price=list_detail[6][2:]
+		print (train_price)
+		train_no=list_detail[3].replace("次列车",'')
+		print (train_no)
+		train_type=list_detail[3][0]
+		if train_type.isdigit():
+			print ("normla")
+		else:
+			print (train_type)
+		start_station=list_detail[2].split('—')[0]
+		print (start_station)
+		stop_station=list_detail[2].split('—')[1]
+		print (stop_station)
+		sit_row=list_detail[4].split("车")[0].replace(" ","")
+		print (sit_row)
+		sit_no=list_detail[4].split("车")[1].split("号")[0]
+		print (sit_no)
+		sit_type=list_detail[5]
+		print (sit_type)
+		transfer_fee=list_detail[7][3:]
+		print (transfer_fee)
+		drawback_fee=list_detail[8][4:]
+		print (drawback_fee)
+		if sit_type=="硬卧":
+			sit_flow=list_detail[4][-2]
+			print (sit_flow)
+
+	elif  '改签' in order_type :
+		line_detail=contents[3].replace("\n",'')
+		list_chick=line_chick.split('，')
+		print(list_chick)
+		if ',' in line_detail:
+			list_detail=line_detail.split(',')
+		elif '，' in line_detail:
+			list_detail=line_detail.split('，')
+		print (list_detail)
+		print ('改签')
+		purchaser='张文'
+		print(purchaser)
+		order_no=list_chick[2].split('。')[1][4:]
+		print(order_no)
+		order_date=list_chick[0][2:13]
+		print (order_date)
+		order_price=list_chick[1][7:]
+		print (order_price)
+		order_type="改签"
+		print (order_type)
+		passenger=list_detail[0].split('.')[1]
+		print (passenger)
+		train_date=list_detail[1][:-1]
+		print (train_date)
+		train_price=list_detail[6][2:]
+		print (train_price)
+		train_no=list_detail[3].replace("次列车",'')
+		print (train_no)
+		train_type=list_detail[3][0]
+		if train_type.isdigit():
+			print ("normla")
+		else:
+			print (train_type)
+		start_station=list_detail[2].split('—')[0]
+		print (start_station)
+		stop_station=list_detail[2].split('—')[1]
+		print (stop_station)
+		sit_row=list_detail[4].split("车")[0]
+		print (sit_row)
+		sit_no=list_detail[4].split("车")[1].split("号")[0]
+		print (sit_no)
+		sit_type=list_detail[5]
+		print (sit_type)
+		if sit_type=="硬卧":
+			sit_flow=list_detail[4][-2]
+			print (sit_flow)
+
+	#print(list_chick)
+	#print (list_detail)
+
+
+def get_subject(num):
+	type,data=raw_conn.fetch(num,'(RFC822)')
+	try:
+		msg=BytesParser().parsebytes(data[0][1])
+		sub=decode_str(msg.get('subject'))
+		print(sub)
+		return sub
+	except TypeError:
+		print ('empty-email')
+	except UnicodeDecodeError:
+		print ('hahah')
+def get_from(num):
+	type,data=raw_conn.fetch(num,'(RFC822)')
+	try :
+		msg=BytesParser().parsebytes(data[0][1])
+		sub=decode_str(msg.get('From'))
+		print (sub)
+		return sub
+
+	except TypeError:
+		print ('empty-email')
+		#print(sub)			
+	except UnicodeDecodeError:
+		print ('hahah')
+def get_date(num):
+	type,data=raw_conn.fetch(num,'(RFC822)')
+	try :
+		msg=BytesParser().parsebytes(data[0][1])
+		sub=msg.get('Date')
+		print(sub)
+		return sub
+	#print(num, decode_str(sub)) 
+	except TypeError:
+		print ('empty-email')
+	except UnicodeDecodeError:
+		print ('hahah')
+
+
+
+
+def get_content(num):
+	print (num)
+	type,data=raw_conn.fetch(num,'(RFC822)')
+	try :
+		msg=BytesParser().parsebytes(data[0][1])
+		for part in msg.walk():
+			if not part.is_multipart():   
+				charset = part.get_charset()
+				contenttype = part.get_content_type()
+				content=part.get_payload(decode=True)
+				content=content.decode('GBK')
+				get_transfer_v1(content)
+				#print (content)
+	except TypeError:
+		print ('empty-email')
+	except UnicodeDecodeError:
+		print ('hahah')
+
+
+
+
+'''if __name__ == '__main__':
+	for count in email_list:
+		#print("VVVVVVVV")
+		print(int(count))
+		get_date(email_list[int(count)])
+		org_addrs=str(get_from(email_list[int(count)]))
+		print (isinstance(org_addrs,str))
+		tar_addrs="<12306@rails.com.cn>"
+		if tar_addrs in  org_addrs:
+			aaaa=aaaa+1
+			print("OOOOOO")
+			get_content(email_list[int(count)])
+			print ("+++++++++++="+str(aaaa))
+			continue'''
+
+get_date(email_list[173])
+get_from(email_list[173])
+get_content(email_list[173])
+
+	
+
+
+
+				#get_content(email_list[30])`
+
+		
+
+
+
+
+		#print (isinstance(org_addrs,str))
+		#if org_addrs==tar_addrs:
+		#	print ('222')
+		#else:
+		#	print("111")
+
+		
+	#get_date(email_list[21])
+	#get_subject(email_list[21])
+	#get_content(email_list[21])
+	#get_subject(newlist[3],raw_conn)
+		#for part in msg.walk():
+		#	if not part.is_multipart():
+		#		print(part.get_payload(decode=True).decode('utf-8'))
+	#raw_conn.close()
+	#raw_conn.logout()
--- etm.py
+++ etm.py
@@ -2,9 +2,11 @@ import sqlite3
 import os,email,imaplib
 from email.parser import BytesParser
 from email.utils import parseaddr 
-from lxml import etree
-import time
-import xml.etree.ElementTree as ET
+import time,re
+import datetime
+from script import Transfer
+
+
 
 aaaa=1
 
@@ -17,7 +19,6 @@ raw_conn.login(user,passwd)
 raw_conn.select()
 type,data=raw_conn.search(None,'ALL')
 email_list=data[0].split()
-print (len(email_list))
 
 def decode_str(s):
 	try:
@@ -34,176 +35,7 @@ def decode_str(s):
 		subject=str(sub_bytes,sub_charset)
 	return subject
 
-def get_transfer_v1(content):
-	a1=content.replace('&nbsp;','')
-	a2=a1.replace('<a <a ','<a ')
-	a3=a2.replace('(<a href="http://www.12306.cn">www.12306.cn</a>)','')
-	a4=a3.replace('（<a href="http://www.12306.cn">www.12306.cn</a>)','')
-	a5=a4.replace('（<a href="http://www.12306.cn">12306.cn</a>)','')
-	a6=a5.replace('(<a href="http://www.12306.cn">12306.cn</a>)','')
-	tree=etree.HTML(a6)
-	contents=tree.xpath('//text()')
-	''.join(contents)
-	line_chick=contents[2].replace("\n",'')
-	list_chick=line_chick.split('，')
-	print (line_chick)
-	order_type=list_chick[0]
-	if'购买' in order_type :
-		order_count=int(list_chick[0][-4])
-		if order_count==2:
-			print ('!!!!!!!!!!!!!!!!!!!!!!!!!')
-		count =1
-		while count < order_count+1:
 
-			line_detail=contents[count+2].replace("\n",'')
-			list_chick=line_chick.split('，')
-			print(list_chick)
-			if ',' in line_detail:
-				list_detail=line_detail.split(',')
-			elif '，' in line_detail:
-				list_detail=line_detail.split('，')
-			print (list_detail)
-			print ("购买")
-			purchaser='张文'
-			print(purchaser)
-			order_no=list_chick[2][4:].split('。')[0]
-			print(order_no)
-			order_date=list_chick[0][2:13]
-			print (order_date)
-			order_price=list_chick[1][4:]
-			print (order_price)
-			order_type="购买"
-			print (order_type)
-			passenger=list_detail[0].split('.')[1]
-			print (passenger)
-			train_date=list_detail[1][:-1]
-			print (train_date)
-			train_price=list_detail[6][2:]
-			print (train_price)
-			train_no=list_detail[3].replace("次列车",'')
-			print (train_no)
-			train_type=list_detail[3][0]
-			if train_type.isdigit():
-				print ("normla")
-			else:
-				print (train_type)
-			start_station=list_detail[2].split('—')[0]
-			print (start_station)
-			stop_station=list_detail[2].split('—')[1]
-			print (stop_station)
-			sit_row=list_detail[4].split("车")[0]
-			print (sit_row)
-			sit_no=list_detail[4].split("车")[1].split("号")[0]
-			print (sit_no)
-			sit_type=list_detail[5]
-			print (sit_type)
-			if sit_type=="硬卧":
-				sit_flow=list_detail[4][-2]
-				print (sit_flow)
-			count=count+1
-			
-
-
-	elif '退票' in order_type:
-		line_detail=contents[3].replace("\n",'')
-		list_chick=line_chick.split('，')
-		print(list_chick)
-		if ',' in line_detail:
-			list_detail=line_detail.split(',')
-		elif '，' in line_detail:
-			list_detail=line_detail.split('，')
-		print (list_detail)
-		print ("退票")
-		purchaser='张文'
-		print(purchaser)
-		order_no=list_chick[1].split('。')[0][5:]
-		print(order_no)
-		order_date=list_chick[0][2:13]
-		print (order_date)
-		#order_price=list_chick[1][4:]
-		#print (order_price)
-		order_type="退票"
-		print (order_type)
-		passenger=list_detail[0]
-		print (passenger)
-		train_date=order_date[0:5]+list_detail[1]
-		print (train_date)
-		train_price=list_detail[6][2:]
-		print (train_price)
-		train_no=list_detail[3].replace("次列车",'')
-		print (train_no)
-		train_type=list_detail[3][0]
-		if train_type.isdigit():
-			print ("normla")
-		else:
-			print (train_type)
-		start_station=list_detail[2].split('—')[0]
-		print (start_station)
-		stop_station=list_detail[2].split('—')[1]
-		print (stop_station)
-		sit_row=list_detail[4].split("车")[0].replace(" ","")
-		print (sit_row)
-		sit_no=list_detail[4].split("车")[1].split("号")[0]
-		print (sit_no)
-		sit_type=list_detail[5]
-		print (sit_type)
-		transfer_fee=list_detail[7][3:]
-		print (transfer_fee)
-		drawback_fee=list_detail[8][4:]
-		print (drawback_fee)
-		if sit_type=="硬卧":
-			sit_flow=list_detail[4][-2]
-			print (sit_flow)
-
-	elif  '改签' in order_type :
-		line_detail=contents[3].replace("\n",'')
-		list_chick=line_chick.split('，')
-		print(list_chick)
-		if ',' in line_detail:
-			list_detail=line_detail.split(',')
-		elif '，' in line_detail:
-			list_detail=line_detail.split('，')
-		print (list_detail)
-		print ('改签')
-		purchaser='张文'
-		print(purchaser)
-		order_no=list_chick[2].split('。')[1][4:]
-		print(order_no)
-		order_date=list_chick[0][2:13]
-		print (order_date)
-		order_price=list_chick[1][7:]
-		print (order_price)
-		order_type="改签"
-		print (order_type)
-		passenger=list_detail[0].split('.')[1]
-		print (passenger)
-		train_date=list_detail[1][:-1]
-		print (train_date)
-		train_price=list_detail[6][2:]
-		print (train_price)
-		train_no=list_detail[3].replace("次列车",'')
-		print (train_no)
-		train_type=list_detail[3][0]
-		if train_type.isdigit():
-			print ("normla")
-		else:
-			print (train_type)
-		start_station=list_detail[2].split('—')[0]
-		print (start_station)
-		stop_station=list_detail[2].split('—')[1]
-		print (stop_station)
-		sit_row=list_detail[4].split("车")[0]
-		print (sit_row)
-		sit_no=list_detail[4].split("车")[1].split("号")[0]
-		print (sit_no)
-		sit_type=list_detail[5]
-		print (sit_type)
-		if sit_type=="硬卧":
-			sit_flow=list_detail[4][-2]
-			print (sit_flow)
-
-	#print(list_chick)
-	#print (list_detail)
 
 
 def get_subject(num):
@@ -222,7 +54,7 @@ def get_from(num):
 	try :
 		msg=BytesParser().parsebytes(data[0][1])
 		sub=decode_str(msg.get('From'))
-		print (sub)
+		#print (sub)
 		return sub
 
 	except TypeError:
@@ -235,7 +67,7 @@ def get_date(num):
 	try :
 		msg=BytesParser().parsebytes(data[0][1])
 		sub=msg.get('Date')
-		print(sub)
+		#print(sub)
 		return sub
 	#print(num, decode_str(sub)) 
 	except TypeError:
@@ -243,12 +75,18 @@ def get_date(num):
 	except UnicodeDecodeError:
 		print ('hahah')
 
-
-
-
+def time_formate(email_time):
+	mind_befor_CTS_time='Fri 10 Nov 2017 00:00:00 '
+	mind_formate_time=datetime.datetime.strptime(mind_befor_CTS_time,"%a %d %b %Y %H:%M:%S ")	
+	email_recive_CTS_time=email_time.replace("(CST)","").replace(",","").replace(" +0800","")
+	email_formate_time=datetime.datetime.strptime(email_recive_CTS_time,"%a %d %b %Y %H:%M:%S ")
+	if email_formate_time >= mind_formate_time:
+		return '2'
+	else:
+		return '1'
 def get_content(num):
-	print (num)
 	type,data=raw_conn.fetch(num,'(RFC822)')
+	email_date=get_date(email_list[int(count)])
 	try :
 		msg=BytesParser().parsebytes(data[0][1])
 		for part in msg.walk():
@@ -257,8 +95,18 @@ def get_content(num):
 				contenttype = part.get_content_type()
 				content=part.get_payload(decode=True)
 				content=content.decode('GBK')
-				get_transfer_v1(content)
+				temp=time_formate(email_date)
+				if temp=='1':
+					print('该邮件使用的老的内容，现使用V1版本进行解析')
+					Transfer.get_transfer_v1(content)
+				elif temp=='2':
+					print('该邮件使用的新的内容，现使用V2版本进行解析')
+					Transfer.get_transfer_v2(content)
+
+				
+				#
 				#print (content)
+
 	except TypeError:
 		print ('empty-email')
 	except UnicodeDecodeError:
@@ -271,20 +119,41 @@ def get_content(num):
 	for count in email_list:
 		#print("VVVVVVVV")
 		print(int(count))
-		get_date(email_list[int(count)])
 		org_addrs=str(get_from(email_list[int(count)]))
-		print (isinstance(org_addrs,str))
+		#print (isinstance(org_addrs,str))
 		tar_addrs="<12306@rails.com.cn>"
 		if tar_addrs in  org_addrs:
-			aaaa=aaaa+1
+			#aaaa=aaaa+1
 			print("OOOOOO")
 			get_content(email_list[int(count)])
-			print ("+++++++++++="+str(aaaa))
+			
+			#print ("+++++++++++="+str(aaaa))
 			continue'''
 
-get_date(email_list[173])
-get_from(email_list[173])
-get_content(email_list[173])
+if __name__ == '__main__':
+	count = 1
+	while count < 528:
+		print("******************part_1*********************")
+		print("******************part_1*********************")
+		print("******************part_1*********************")
+		print("******************part_1*********************")
+		print("******************part_1*********************")
+		print("******************part_1*********************")
+		print("******************part_1*********************")
+		print('现在开始查询第'+str(count)+'封邮件')
+		org_addrs=str(get_from(email_list[int(count)]))
+		#print (isinstance(org_addrs,str))
+		tar_addrs="<12306@rails.com.cn>"
+		if tar_addrs in  org_addrs:
+			print("已匹配到12306邮件，下一步解析该邮件")
+			get_content(email_list[int(count)])
+		count=count+1
+		print("此邮件解析完毕了")
+		print("~~~~~~~~~~~~~~~~~end~~~~~~~~~~~~~~~~~~~~~~~")
+
+#
+#get_from(email_list[330])
+#get_content(email_list[173])
 
 	
 
